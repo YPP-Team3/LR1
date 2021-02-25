4b0f3c71678b42b873f5e4d77eb5a0e6af92301a
zhenya9876 <zhenya9809@mail.ru>
2021-02-26 00:47:31 +0500

    Реализовал асинхронные методы чтения и записи из Json файла. Реализовал класс пользовательского фильтра.

---
+++ UserFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    class UserFilter
+    {
+        public string UserId { get; set; }
+        public uint Views { get; set; }
+        public uint Likes { get; set; }
+        public enum FilterType
+        {
+            Youtube, Instagram
+        }
+        public FilterType Filter { get; set; }
+        public UserFilter(string userId, uint views, uint likes, FilterType filter)
+        {
+            UserId = userId;
+            Views = views;
+            Likes = likes;
+            Filter = filter;
+        }
+
+        public override string ToString()
+        {
+            return $"[{UserId}]'s {Filter} filter contains ({Views}) views and ({Likes}) likes";
+        }
+    }
+}
