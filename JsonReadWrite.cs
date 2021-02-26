1da6f32db88dfe33bd89a900c77bc4523e06f88f
zhenya9876 <zhenya9809@mail.ru>
2021-02-26 15:25:33 +0500

    Заменил Thread.Sleep на Task.Delay в JsonReadWrite и добавил *.sqlite в .gitignore ()

--- JsonReadWrite.cs
+++ JsonReadWrite.cs
@@ -25,7 +25,7 @@ namespace TelegramBot
                 }
                 catch (IOException e) when (i <= NumberOfRetries)
                 {
-                    Thread.Sleep(DelayOnRetry);
+                    Task.Delay(DelayOnRetry);
                 }
             }
         }
@@ -43,7 +43,7 @@ namespace TelegramBot
                 }
                 catch (IOException e) when (i <= NumberOfRetries)
                 {
-                    Thread.Sleep(DelayOnRetry);
+                    Task.Delay(DelayOnRetry);
                 }
             }
             return userFilters;   
